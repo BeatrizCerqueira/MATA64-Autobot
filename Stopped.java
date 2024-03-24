5ae8b11442a23d27b5020fe9ab0174f4c705f47c
BeatrizCerqueira <beatriz_cbj@hotmail.com>
2024-03-24 03:58:40 -0300

    Add Stopped and TurnGunAndFire robots

---
+++ Stopped.java
@@ -0,0 +1,19 @@
+package autobot;
+
+import robocode.Robot;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html 
+
+/**
+ * Stopped - a robot by (your name here)
+ */
+public class Stopped extends Robot {
+    /**
+     * run: Stopped's default behavior
+     */
+    @Override
+    public void run() {
+
+    }
+
+}
