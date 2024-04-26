3b2bc54e23a9ac50216b752438c401427523559f
BeatrizCerqueira <beatriz_cbj@hotmail.com>
2024-04-26 00:26:15 -0300

    refact other fn onScannedRobot

--- v1/lucas/Log.java
+++ v1/lucas/Log.java
@@ -1,22 +1,22 @@
-package autobot.v1;
-
-import org.jpl7.Query;
-import robocode.Robot;
-
-public class Log extends Robot {
-    public void run() {
-        if (!Query.hasSolution("consult('test.pl').")) {
-            System.out.println("Consult failed");
-
-        }
-
-        while (true) {
-            // Replace the next 4 lines with any behavior you would like
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-        }
-    }
-
-}
+//package autobot.v1;
+//
+//import org.jpl7.Query;
+//import robocode.Robot;
+//
+//public class Log extends Robot {
+//    public void run() {
+//        if (!Query.hasSolution("consult('test.pl').")) {
+//            System.out.println("Consult failed");
+//
+//        }
+//
+//        while (true) {
+//            // Replace the next 4 lines with any behavior you would like
+//            ahead(100);
+//            turnGunRight(360);
+//            back(100);
+//            turnGunRight(360);
+//        }
+//    }
+//
+//}
