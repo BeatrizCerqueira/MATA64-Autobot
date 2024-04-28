dfbe7b56da04d822ac0989de020e3e6471e29198
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2024-04-28 15:36:16 -0300

    add enemy new methods

--- v1/updated/Enemy.java
+++ v1/updated/Enemy.java
@@ -1,6 +1,7 @@
 package autobot.v1.updated;
 
 import autobot.v1.updated.aux.Consts;
+import autobot.v1.updated.aux.MathUtils;
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 
@@ -15,6 +16,7 @@ public class Enemy {
     double angle;
     double distance;
 
+
     public Enemy() {
         energy = Consts.INITIAL_ENERGY;
         heat = Consts.INITIAL_GUN_HEAT;
@@ -22,14 +24,14 @@ public class Enemy {
 
     public void scanned(AdvancedRobot myBot, ScannedRobotEvent e) {
         //update enemy attributes on scanned event
+
         angle = myBot.getHeading() + e.getBearing();
-        energy = e.getEnergy();
         distance = e.getDistance();
 
+        energy = e.getEnergy();
+        identifyEnemyBullets(energy - e.getEnergy());
 
-//        setEnemyLocation(e);
-//        moveAwayFromEnemy(e);
-//        identifyEnemyBullets(e);
+        setLocation(myBot);
     }
 
     public double getAngle() {
@@ -42,5 +44,20 @@ public class Enemy {
 
     public double getDistance() {
         return distance;
+    }
+
+    private void setLocation(AdvancedRobot myBot) {
+        Point2D myBotLocation = new Point2D.Double(myBot.getX(), myBot.getY());
+        location = MathUtils.getLocation(myBotLocation, getAngleRad(), distance);
+    }
+
+    private void identifyEnemyBullets(double energyDecreased) {
+        // check enemy energy decrease to identify if enemy has fired
+        // if so, update heat
+
+        boolean hasEnemyFired = energyDecreased > 0 && energyDecreased <= 3;
+        if (hasEnemyFired)
+            heat = 1 + (energyDecreased / 5);
+
     }
 }
