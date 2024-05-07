c82ec6097002ba46ebab0f7822ba2b6821bd5f99
Lucas Morais Barreto <lucas.m.barreto12@gmail.com>
2024-05-07 17:09:19 -0300

    prolog with strings

--- v1/drafts/lucas/Prolog.java
+++ v1/drafts/lucas/Prolog.java
@@ -3,12 +3,65 @@ package autobot.v1.drafts.lucas;
 import org.jpl7.Query;
 import org.jpl7.Term;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Prolog {
     public static void main(String[] args) {
+        test();
+    }
 
+    private static String queryBuilder(String ruleName, String... values) {
+        String valuesStr = String.join(", ", values);
+        return String.format("%s(%s)", ruleName, valuesStr);
+    }
+
+    private static String queryBuilderWithVars(String ruleName, String... values) {
+        String valuesStr;
+        if (values.length == 0) {
+            valuesStr = "X";
+        } else {
+            valuesStr = String.join(", ", values).replace("null", "X");
+        }
+        return String.format("%s(%s)", ruleName, valuesStr);
+    }
+
+    private static void addFact(String ruleName, String... values) {
+        String fact = queryBuilder(ruleName, values);
+        String query = String.format("assert(%s)", fact);
+        System.out.println(query);
+        Query q = new Query(query);
+        q.hasSolution();
+    }
+
+    private static boolean isValid(String ruleName, String... values) {
+        String query = queryBuilder(ruleName, values);
+        System.out.print(query + " = ");
+        Query q = new Query(query);
+        return q.hasSolution();
+    }
+
+    private static String getOneSolution(String ruleName, String... values) {
+        String query = queryBuilderWithVars(ruleName, values);
+        System.out.print(query + " = ");
+        Query q = new Query(query);
+        return q.hasSolution() ? q.oneSolution().get("X").toString() : null;
+    }
+
+    private static List<String> getAllSolutions(String ruleName, String... values) {
+        List<String> solutions = new ArrayList<>();
+        String query = queryBuilderWithVars(ruleName, values);
+        System.out.print(query + " = ");
+        Query q = new Query(query);
+        for (Map<String, Term> solution : q.allSolutions())
+            solutions.add(solution.get("X").toString());
+        return solutions;
+    }
+
+    // ============================ TEST ============================
+
+    static void test() {
         System.out.println("\n=================================");
         System.out.println("Adding facts...");
 
@@ -27,6 +80,9 @@ public class Prolog {
         System.out.println(isValid("amigos", "lucas", "bia"));
         System.out.println(isValid("amigos", "gabriel", "bia"));
 
+        System.out.println(isValid("amigos", "lucas", "gabriel"));
+        System.out.println(isValid("amigos", "gabriel", "lucas"));
+
         System.out.println(isValid("feliz", "bia"));
         System.out.println(isValid("feliz", "lucas"));
         System.out.println(isValid("feliz", "gabriel"));
@@ -42,7 +98,7 @@ public class Prolog {
         System.out.println(getOneSolution("amigos", "lucas", null));
         System.out.println(getOneSolution("amigos", "gabriel", null));
 
-        System.out.println(getOneSolution("feliz", (String) null));
+        System.out.println(getOneSolution("feliz"));
 
         System.out.println("\n=================================");
         System.out.println("\nTesting getAllSolutions...");
@@ -55,50 +111,9 @@ public class Prolog {
         System.out.println(getAllSolutions("amigos", "lucas", null));
         System.out.println(getAllSolutions("amigos", "gabriel", null));
 
-        System.out.println(getAllSolutions("feliz", (String) null));
-
+        System.out.println(getAllSolutions("feliz"));
     }
 
-
-    // TODO: Testar se mais de um null
-    // TODO: Testar se número errado de parâmetros para aquela ruleName
-    private static String queryBuilder(String ruleName, String... values) {
-        String valuesStr = String.join(", ", values).replace("null", "X");
-        return String.format("%s(%s)", ruleName, valuesStr);
-    }
-
-    private static void addFact(String ruleName, String... values) {
-        String fact = queryBuilder(ruleName, values);
-        String query = String.format("assert(%s)", fact);
-        System.out.println(query);
-//        Query q = new Query(query);
-//        q.hasSolution();
-    }
-
-    private static boolean isValid(String ruleName, String... values) {
-        String query = queryBuilder(ruleName, values);
-        System.out.print(query + " = ");
-//        Query q = new Query(query);
-//        return q.hasSolution();
-        return true;
-    }
-
-    private static String getOneSolution(String ruleName, String... values) {
-        String query = queryBuilder(ruleName, values);
-        System.out.print(query + " = ");
-//        Query q = new Query(query);
-//        return q.oneSolution() [...]
-        return "foo";
-    }
-
-
-    private static List<String> getAllSolutions(String ruleName, String... values) {
-        String query = queryBuilder(ruleName, values);
-        System.out.print(query + " = ");
-//        Query q = new Query(query);
-//        return q.allSolutions() [...]
-        return Arrays.asList("foo", "bar");
-    }
 
     // ============================= OLD =============================
 
@@ -115,7 +130,6 @@ public class Prolog {
                 new org.jpl7.Float(EnemyDistance),
                 new org.jpl7.Float(LimitDistance)
         };
-
         return hasSolution("isEnemyClose", terms);
     }
 
