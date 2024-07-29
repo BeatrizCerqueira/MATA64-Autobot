3ce58eaea6c031a0fe951360efd3428fd45e5ff9
BeatrizCerqueira <beatriz_cbj@hotmail.com>
2024-07-29 05:33:30 -0300

    AgoraVai com Jayes ok

--- _studying/bayes/AgoraVai.java
+++ _studying/bayes/AgoraVai.java
@@ -19,6 +19,9 @@
 
 package autobot._studying.bayes;
 
+import org.eclipse.recommenders.jayes.BayesNet;
+import org.eclipse.recommenders.jayes.BayesNode;
+import org.eclipse.recommenders.jayes.util.MathUtils;
 import weka.classifiers.bayes.net.EditableBayesNet;
 import weka.classifiers.bayes.net.MarginCalculator;
 import weka.core.Attribute;
@@ -54,20 +57,19 @@ class Weka {
     Instances dataset;
     EditableBayesNet bayesNet;
     MarginCalculator marginalDistributionCalculator;
-    List<InternalBayesNode> nodes;
+    List<InternalBayesNode> internalNodes;
 
-    public Weka(List<InternalBayesNode> nodes) throws Exception {
-        this.nodes = nodes;
+    public Weka(List<InternalBayesNode> internalNodes) throws Exception {
+        this.internalNodes = internalNodes;
         initDataset();
         initBayesianNetwork();
         initMarginCalculator();
-        printInit();
     }
 
     private void initDataset() {
         ArrayList<Attribute> attributes = new ArrayList<>();
-        for (InternalBayesNode node : nodes) {
-            attributes.add(new Attribute(node.name, node.values));
+        for (InternalBayesNode internalNode : internalNodes) {
+            attributes.add(new Attribute(internalNode.name, internalNode.values));
         }
         Instances dataset = new Instances("Dataset", attributes, 0);
         dataset.setClassIndex(dataset.numAttributes() - 1);
@@ -76,10 +78,10 @@ class Weka {
 
     private void initBayesianNetwork() throws Exception {
         EditableBayesNet bayesNet = new EditableBayesNet(dataset);
-        for (InternalBayesNode node : nodes) {
-            if (!node.parents.isEmpty()) {
-                for (String parent : node.parents) {
-                    bayesNet.addArc(parent, node.name);
+        for (InternalBayesNode internalNode : internalNodes) {
+            if (!internalNode.parents.isEmpty()) {
+                for (String parent : internalNode.parents) {
+                    bayesNet.addArc(parent, internalNode.name);
                 }
             }
         }
@@ -102,26 +104,26 @@ class Weka {
         System.out.println(bayesNet);
 
         System.out.println("Attributes with values:");
-        for (InternalBayesNode node : nodes) {
-            System.out.println(node.name + ": " + Arrays.toString(bayesNet.getValues(node.name)));
+        for (InternalBayesNode internalNode : internalNodes) {
+            System.out.println(internalNode.name + ": " + Arrays.toString(bayesNet.getValues(internalNode.name)));
         }
     }
 
     private void printMarginalDistribution() {
         System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>> Marginal Distribution <<<<<<<<<<<<<<<<<<<<<<<<<<");
-        for (int i = 0; i < nodes.size(); i++) {
-            System.out.println(nodes.get(i).name + ": " + Arrays.toString(marginalDistributionCalculator.getMargin(i)));
+        for (int i = 0; i < internalNodes.size(); i++) {
+            System.out.println(internalNodes.get(i).name + ": " + Arrays.toString(marginalDistributionCalculator.getMargin(i)));
         }
     }
 
     private void printConditionalDistribution() {
         System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>> Conditional Distribution <<<<<<<<<<<<<<<<<<<<<<<<<<");
-        for (InternalBayesNode node : nodes) {
-            System.out.println(node.name + ": " + Arrays.deepToString(bayesNet.getDistribution(node.name)));
+        for (InternalBayesNode internalNode : internalNodes) {
+            System.out.println(internalNode.name + ": " + Arrays.deepToString(bayesNet.getDistribution(internalNode.name)));
         }
     }
 
-    private void printInit() {
+    public void printInit() {
         System.out.println("\n\n\n========================================= Initial network =========================================");
         printAll();
     }
@@ -135,19 +137,73 @@ class Weka {
 
 }
 
+class Jayes {
+    BayesNet bayesNet;
+    List<InternalBayesNode> internalNodes;
+
+    public Jayes(List<InternalBayesNode> internalNodes) {
+        this.internalNodes = internalNodes;
+        initBayesianNetwork();
+    }
+
+    private void initBayesianNetwork() {
+        this.bayesNet = new BayesNet();
+        initNodes();
+        initParents();
+    }
+
+    private void initNodes() {
+        for (InternalBayesNode internalNode : internalNodes) {
+            BayesNode jayesNode = bayesNet.createNode(internalNode.name);
+            jayesNode.addOutcomes(internalNode.values.toArray(new String[0]));
+        }
+    }
+
+    private void initParents() {
+        for (InternalBayesNode internalNode : internalNodes) {
+            if (!internalNode.parents.isEmpty()) {
+                BayesNode jayesNode = bayesNet.getNode(internalNode.name);
+                List<BayesNode> parents = new ArrayList<>();
+                for (String parent : internalNode.parents) {
+                    parents.add(bayesNet.getNode(parent));
+                }
+                jayesNode.setParents(parents);
+            }
+        }
+    }
+
+    public void printInit() {
+        System.out.println("\n\n\n========================================= Initial network =========================================");
+        printAll();
+    }
+
+    public void printAll() {
+        for (BayesNode jayesNode : bayesNet.getNodes()) {
+            System.out.println();
+            System.out.println("Node: " + jayesNode);
+            System.out.println("Values:" + jayesNode.getOutcomes());
+            System.out.println("Parents:" + jayesNode.getParents());
+            System.out.println("Expected # of probabilities: " + MathUtils.product(jayesNode.getFactor().getDimensions()));
+        }
+    }
+}
+
 public class AgoraVai {
 
     public static void main(String[] args) throws Exception {
-        List<InternalBayesNode> nodes = new ArrayList<>();
+        List<InternalBayesNode> internalNodes = new ArrayList<>();
 
-        nodes.add(new InternalBayesNode("EnemyDistance", Arrays.asList("dist1", "dist2"), new ArrayList<>()));
-        nodes.add(new InternalBayesNode("EnemyVelocity", Arrays.asList("sp1", "sp2"), new ArrayList<>()));
-        nodes.add(new InternalBayesNode("EnemyAngle", Arrays.asList("ea1", "ea2"), new ArrayList<>()));
-        nodes.add(new InternalBayesNode("MyGunAngle", Arrays.asList("mga1", "mga2"), new ArrayList<>()));
-        nodes.add(new InternalBayesNode("FirePower", Arrays.asList("fp1", "fp2"), new ArrayList<>()));
-        nodes.add(new InternalBayesNode("Hit", Arrays.asList("true", "false"), Arrays.asList("EnemyDistance", "EnemyVelocity", "EnemyAngle", "MyGunAngle", "FirePower")));
-        
-        Weka weka = new Weka(nodes);
+        internalNodes.add(new InternalBayesNode("EnemyDistance", Arrays.asList("dist1", "dist2"), new ArrayList<>()));
+        internalNodes.add(new InternalBayesNode("EnemyVelocity", Arrays.asList("sp1", "sp2"), new ArrayList<>()));
+        internalNodes.add(new InternalBayesNode("EnemyAngle", Arrays.asList("ea1", "ea2"), new ArrayList<>()));
+        internalNodes.add(new InternalBayesNode("MyGunAngle", Arrays.asList("mga1", "mga2"), new ArrayList<>()));
+        internalNodes.add(new InternalBayesNode("FirePower", Arrays.asList("fp1", "fp2"), new ArrayList<>()));
+        internalNodes.add(new InternalBayesNode("Hit", Arrays.asList("true", "false"), Arrays.asList("EnemyDistance", "EnemyVelocity", "EnemyAngle", "MyGunAngle", "FirePower")));
+
+        Weka weka = new Weka(internalNodes);
+        weka.printInit();
+        Jayes jayes = new Jayes(internalNodes);
+        jayes.printInit();
 
     }
 }
