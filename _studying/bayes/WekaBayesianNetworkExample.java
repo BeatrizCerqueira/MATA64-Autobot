8a2a17c54c6bf260cd56b9d64b8af3b4bab4a9bf
BeatrizCerqueira <beatriz_cbj@hotmail.com>
2024-07-29 02:07:35 -0300

    refactor still working with methods

--- _studying/bayes/WekaBayesianNetworkExample.java
+++ _studying/bayes/WekaBayesianNetworkExample.java
@@ -11,78 +11,87 @@ import java.util.Arrays;
 
 public class WekaBayesianNetworkExample {
     public static void main(String[] args) throws Exception {
-        // Define attributes
+        Instances dataset = createDataset();
+        EditableBayesNet bayesNet = createBayesianNetwork(dataset);
+        MarginCalculator marginCalculator = createMarginCalculator(bayesNet);
+
+        System.out.println("\n\n\n========================================= Initial network =========================================");
+        printNetworkInfo(bayesNet, marginCalculator, dataset);
+
+        addInstances(dataset, bayesNet, marginCalculator);
+
+        System.out.println("\n\n\n========================================= Updated network =========================================");
+        printNetworkInfo(bayesNet, marginCalculator, dataset);
+    }
+
+    private static MarginCalculator createMarginCalculator(EditableBayesNet bayesNet) throws Exception {
+        MarginCalculator marginCalculator = new MarginCalculator();
+        marginCalculator.calcMargins(bayesNet);
+        return marginCalculator;
+    }
+
+    private static ArrayList<Attribute> defineAttributes() {
         ArrayList<Attribute> attributes = new ArrayList<>();
-        ArrayList<String> manufacturerValues = new ArrayList<>();
-        manufacturerValues.add("Dell");
-        manufacturerValues.add("Compaq");
-        manufacturerValues.add("Gateway");
+        ArrayList<String> manufacturerValues = new ArrayList<>(Arrays.asList("Dell", "Compaq", "Gateway"));
         attributes.add(new Attribute("Manufacturer", manufacturerValues));
 
-        ArrayList<String> osValues = new ArrayList<>();
-        osValues.add("Windows");
-        osValues.add("Linux");
+        ArrayList<String> osValues = new ArrayList<>(Arrays.asList("Windows", "Linux"));
         attributes.add(new Attribute("OS", osValues));
 
-        ArrayList<String> symptomValues = new ArrayList<>();
-        symptomValues.add("Can't print");
-        symptomValues.add("No display");
+        ArrayList<String> symptomValues = new ArrayList<>(Arrays.asList("Can't print", "No display"));
         attributes.add(new Attribute("Symptom", symptomValues));
 
-        ArrayList<String> causeValues = new ArrayList<>();
-        causeValues.add("Driver");
-        causeValues.add("Hardware");
+        ArrayList<String> causeValues = new ArrayList<>(Arrays.asList("Driver", "Hardware"));
         attributes.add(new Attribute("Cause", causeValues));
 
-        // Create dataset
+        return attributes;
+    }
+
+    private static Instances createDataset() {
+        ArrayList<Attribute> attributes = defineAttributes();
         Instances dataset = new Instances("Dataset", attributes, 0);
         dataset.setClassIndex(dataset.numAttributes() - 1);
+        return dataset;
+    }
 
-        // Create Bayesian network
+    private static EditableBayesNet createBayesianNetwork(Instances dataset) throws Exception {
         EditableBayesNet bayesNet = new EditableBayesNet(dataset);
-
         bayesNet.addArc("Cause", "Manufacturer");
         bayesNet.addArc("Cause", "OS");
         bayesNet.addArc("Cause", "Symptom");
+        return bayesNet;
+    }
 
-        // Create margin distribution calculator
-        MarginCalculator marginCalculator = new MarginCalculator();
-        marginCalculator.calcMargins(bayesNet);
-
-        // Print network before adding instances
+    private static void printNetworkInfo(EditableBayesNet bayesNet, MarginCalculator marginCalculator, Instances dataset) throws Exception {
         System.out.println(bayesNet);
-
         System.out.println("\nManufacturer index: " + bayesNet.getNode("Manufacturer"));
         System.out.println(Arrays.toString(bayesNet.getValues("Manufacturer")));
-
         System.out.println("\nOS index: " + bayesNet.getNode("OS"));
         System.out.println(Arrays.toString(bayesNet.getValues("OS")));
-
         System.out.println("\nSymptom index: " + bayesNet.getNode("Symptom"));
         System.out.println(Arrays.toString(bayesNet.getValues("Symptom")));
-
         System.out.println("\nCause index: " + bayesNet.getNode("Cause"));
         System.out.println(Arrays.toString(bayesNet.getValues("Cause")));
-
-        // Print dataset before adding instances
         System.out.println();
         System.out.println(dataset);
-
-        // Print marginal distributions before adding instances
         System.out.println();
         System.out.println(Arrays.toString(marginCalculator.getMargin(0)));
         System.out.println(Arrays.toString(marginCalculator.getMargin(1)));
         System.out.println(Arrays.toString(marginCalculator.getMargin(2)));
         System.out.println(Arrays.toString(marginCalculator.getMargin(3)));
-
-        // Print conditional distributions before adding instances
         System.out.println();
         System.out.println(Arrays.deepToString(bayesNet.getDistribution("Manufacturer")));
         System.out.println(Arrays.deepToString(bayesNet.getDistribution("OS")));
         System.out.println(Arrays.deepToString(bayesNet.getDistribution("Symptom")));
         System.out.println(Arrays.deepToString(bayesNet.getDistribution("Cause")));
+    }
 
-        // Add instances
+    private static void addInstances(Instances dataset, EditableBayesNet bayesNet, MarginCalculator marginCalculator) throws Exception {
+        ArrayList<String> manufacturerValues = new ArrayList<>(Arrays.asList("Dell", "Compaq", "Gateway"));
+        ArrayList<String> osValues = new ArrayList<>(Arrays.asList("Windows", "Linux"));
+        ArrayList<String> symptomValues = new ArrayList<>(Arrays.asList("Can't print", "No display"));
+        ArrayList<String> causeValues = new ArrayList<>(Arrays.asList("Driver", "Hardware"));
+
         double[] instance1 = new double[]{manufacturerValues.indexOf("Dell"), osValues.indexOf("Windows"), symptomValues.indexOf("Can't print"), causeValues.indexOf("Driver")};
         double[] instance2 = new double[]{manufacturerValues.indexOf("Compaq"), osValues.indexOf("Linux"), symptomValues.indexOf("Can't print"), causeValues.indexOf("Driver")};
         double[] instance3 = new double[]{manufacturerValues.indexOf("Dell"), osValues.indexOf("Linux"), symptomValues.indexOf("No display"), causeValues.indexOf("Hardware")};
@@ -93,28 +102,8 @@ public class WekaBayesianNetworkExample {
         dataset.add(new DenseInstance(1.0, instance3));
         dataset.add(new DenseInstance(1.0, instance4));
 
-        // Update network
         bayesNet.setData(dataset);
         bayesNet.estimateCPTs();
         marginCalculator.calcMargins(bayesNet);
-
-        // Print dataset after adding instances
-        System.out.println();
-        System.out.println(dataset);
-
-        // Print marginal distributions after adding instances
-        System.out.println();
-        System.out.println(Arrays.toString(marginCalculator.getMargin(0)));
-        System.out.println(Arrays.toString(marginCalculator.getMargin(1)));
-        System.out.println(Arrays.toString(marginCalculator.getMargin(2)));
-        System.out.println(Arrays.toString(marginCalculator.getMargin(3)));
-
-        // Print conditional distributions after adding instances
-        System.out.println();
-        System.out.println(Arrays.deepToString(bayesNet.getDistribution("Manufacturer")));
-        System.out.println(Arrays.deepToString(bayesNet.getDistribution("OS")));
-        System.out.println(Arrays.deepToString(bayesNet.getDistribution("Symptom")));
-        System.out.println(Arrays.deepToString(bayesNet.getDistribution("Cause")));
-
     }
 }
